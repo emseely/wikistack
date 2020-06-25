012b27051079e4048c09c44852f025287a2a8dbf
Ashlee Pitock <ashlee.pitock@gmail.com>
2020-06-25 15:08:21 -0400

    schema done

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 const express = require("express");
 const morgan = require("morgan");
-
+const models = require('./models');
 const app = express();
+const PORT = 3000;
 
 app.use(morgan("dev"));
-app.use(express.static('public'));
+app.use(express.static('/public'));
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   res.send(console.log('heeeyyyy world'));
 });
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log('wasup')
-})
+const init = async () => {
+  try {
+    await models.db.sync();
+    app.listen(PORT, () => {
+      console.log('wasup')
+    })
+  } catch(e) {
+    res.send('Nah', e);
+  }
+}
+init()
+
