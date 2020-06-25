85850c24e7c751a78321e835d9c2b3436751d3b2
Ashlee Pitock <ashlee.pitock@gmail.com>
2020-06-25 12:46:27 -0400

    added nodemon

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ const morgan = require("morgan");
 const app = express();
 
 app.use(morgan("dev"));
-app.use(express.static(__dirname + "/public"));
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.send(console.log("heeeyyyy world"));
+  res.send(console.log('heeeyyyy world'));
 });
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log('wasup')
+})
